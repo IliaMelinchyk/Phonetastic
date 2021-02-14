42bdedaf38eb2b67bbca9a2dd6a9f294491617d8
IliaMelinchyk <sm1tphoen1x@gmail.com>
2021-02-15 01:50:26 +0300

    1

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+import classes from "./Layout.scss";
+const layout = (props) => (
+  <>
+    <div>Toolbar,SideDrawer,Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </>
+);
+export default layout;
